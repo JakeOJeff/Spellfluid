e8387b52a93c827e52b4af6c51bbca1075626a41
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-12 04:25:11 +0530

    Advent algorithm Setup

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 -- GLOBALS
 
-N = 24 -- resolution
+N = 24   -- resolution
 ITER = 8 -- Gauss–Seidel Iteration Method
 
 
@@ -35,48 +35,45 @@ local function bound(b, s, N)
         .                  .
         (1,N)      ...    (N,N)
     ]]
-    for i = 2, (N_- 1) do
-        s[i][1] = ( b == 2 ) and -s[i][2] or s[i][2] -- Bottom Bound
-        s[i][N] = ( b == 2 ) and -s[i][N-1] or s[i][N-1] -- Top Bound
-        s[1][i] = ( b == 1 ) and -s[2][i] or s[2][i] -- Left Bound
-        s[N][i] = ( b == 1 ) and -s[N-1][i] or s[N-1][i] -- Right Bound
+    for i = 2, (N_ - 1) do
+        s[i][1] = (b == 2) and -s[i][2] or s[i][2]       -- Bottom Bound
+        s[i][N] = (b == 2) and -s[i][N - 1] or s[i][N - 1] -- Top Bound
+        s[1][i] = (b == 1) and -s[2][i] or s[2][i]       -- Left Bound
+        s[N][i] = (b == 1) and -s[N - 1][i] or s[N - 1][i] -- Right Bound
     end
 
     -- Corner Cells  = avg of neighbours
-    s[1][1] = 0.5 * ( s[2][1] + s[1][2] )
-    s[1][N] = 0.5 * ( s[2][N] + s[1][N-1] )
-    s[N][1] = 0.5 * ( s[N-1][1] + s[N][2] )
-    s[N][N] = 0.5 * ( s[N-1][N] + s[N][N-1])
-             
+    s[1][1] = 0.5 * (s[2][1] + s[1][2])
+    s[1][N] = 0.5 * (s[2][N] + s[1][N - 1])
+    s[N][1] = 0.5 * (s[N - 1][1] + s[N][2])
+    s[N][N] = 0.5 * (s[N - 1][N] + s[N][N - 1])
 end
 
 
 -- Global force
 function Force(Vx, Vy, Fx, Fy, x, y, radius, dt)
-    
     --[[
-    
+
         Vx, Vy = Velocity Field Array [ Gridwise ]
         Fx, Fy = External Force Factor Parameter
         x, y = Grid Position ( force center )
         radius = Range of influence
-        dt = deltatime 
-    
+        dt = deltatime
+
     ]]
-    
+
     for i = (x - radius), (x + radius) do
         for j = (y - radius), (y + radius) do
             if i > 1 and i < N and j > 1 and j < N then
-                
                 local dx = i - x
                 local dy = j - y
 
-                local distanceSq = dx*dx + dy*dy
+                local distanceSq = dx * dx + dy * dy
                 local radiusSq = radius * radius
                 if distanceSq < radiusSq then
-                    local falloff = exp(-distanceSq / radiusSq ) -- 1 - ( distance / radius ) for linear interpolation
+                    local falloff = exp(-distanceSq / radiusSq)  -- 1 - ( distance / radius ) for linear interpolation
                     Vx[i][j] = Vx[i][j] + Fx * falloff * dt
-                    Vy[i][j] = Vy[i][j] + Fy * falloff * dt 
+                    Vy[i][j] = Vy[i][j] + Fy * falloff * dt
 
                     return Vx, Vy
                 end
@@ -85,40 +82,36 @@ function Force(Vx, Vy, Fx, Fy, x, y, radius, dt)
     end
 end
 
-
 function Diffuse(b, s, Bs, diff, dt, iter, N)
     --[[
         Laplacian ( using Laplacian Operator : 2X)
         ∇2X≈Xi−1,j​+Xi+1,j​+Xi,j−1​+Xi,j+1​−4Xi,j​
 
-        b = boundary type ( 
+        b = boundary type (
             0 - scalar,
             1 - Vx ( Velocity x-axis )
             2 - Vy ( Velocity y-axis )
         )
-        
+
         s = latest diffused state
         Bs = previous diffused state ( source state )
-        iter = number of Gauss-Seidel iterations 
+        iter = number of Gauss-Seidel iterations
     ]]
 
 
-    local a = dt * diff * (N - 2)^2
+    local a = dt * diff * (N - 2) ^ 2
 
     for k = 1, iter do
         for j = 2, N - 1 do
             for i = 2, N - 1 do
-                s[i][j] = (Bs[i][j] + a * ( s[i - 1][j] + s[i + 1][j] + s[i][j - 1] + s[i][j + 1] ) / ( 1 + 4 * a ) )
+                s[i][j] = (Bs[i][j] + a * (s[i - 1][j] + s[i + 1][j] + s[i][j - 1] + s[i][j + 1]) / (1 + 4 * a))
             end
             bound(b, s, N)
         end
     end
-
-
 end
 
 function Advect(b, a, Ba, Vx, Vy, dt, N)
-
     --[[
         / Semi-Lagrangian Advection (Stam’s Method)
 
@@ -129,21 +122,43 @@ function Advect(b, a, Ba, Vx, Vy, dt, N)
 
         bilinear interpolation
 
-        note : this is backward tracing instead of forward integration, 
+        note : this is backward tracing instead of forward integration,
         hence, the grid cell content depends on where it came from rather than
         where it might or will go
 
+
+        function advect(b, d, d0, Vx, Vy, dt, N):
+            dt0 = dt * (N-2)
+            for j = 2 to N-1:
+                for i = 2 to N-1:
+                    x = i - dt0 * Vx[i,j]
+                    y = j - dt0 * Vy[i,j]
+                    x = clamp(x, 1.5, N-0.5)
+                    y = clamp(y, 1.5, N-0.5)
+
+                    i0 = floor(x)
+                    i1 = i0 + 1
+                    j0 = floor(y)
+                    j1 = j0 + 1
+
+                    s1 = x - i0
+                    s0 = 1 - s1
+                    t1 = y - j0
+                    t0 = 1 - t1
+
+                    d[i,j] = s0*(t0*d0[i0,j0] + t1*d0[i0,j1]) +
+                            s1*(t0*d0[i1,j0] + t1*d0[i1,j1])
+            set_bnd(b, d, N)
     ]]
 
-    Bdt = dt * (N-2)
+    Bdt = dt * (N - 2)
 
-    for j = 2, N-1 do
-        for i = 2, N-1 do
-            
-            i0 = floor(clamp((i - Bdt * Vx[i][j]), 1.5, N - 0.5 ))
+    for j = 2, N - 1 do
+        for i = 2, N - 1 do
+            i0 = floor(clamp((i - Bdt * Vx[i][j]), 1.5, N - 0.5))
             i1 = i0 + 1
-
+            j0 = floor(clamp((j - Bdt * Vy[i][j]), 1.5, N - 0.5))
+            j1 = j0 + 1
         end
     end
-
 end
