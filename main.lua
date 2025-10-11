1a83d2601397c9e3c6ba187d1a113e49c6e8b831
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-12 00:19:15 +0530

    Initializing force parameters

---
+++ main.lua
@@ -0,0 +1,11 @@
+-- GLOBALS
+
+N = 24 -- resolution
+ITER = 8 -- Gauss–Seidel Iteration Method
+
+
+-- NOTE FROM CREATOR : x and y will be represented as i and j to sync with the formulas
+
+function force(Vi, Vj, Fi, Fj, x, y, radius, dt)
+    
+end
