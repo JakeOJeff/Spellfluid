65bde6887f0e2765cb3257862c73053547cc831d
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-13 02:27:10 +0530

    Circle creation on reset

--- main.lua
+++ main.lua
@@ -5,14 +5,12 @@ local fluid = require "fluid"
 local mouseDensity = 100
 
 function love.load()
-
     wW, wH = 720, 720
-    size = wW / N 
+    size = wW / N
     fluid:init()
-    fluid:solidCircle(N/2, N/2, 10)
+    fluid:solidCircle(N / 2, N / 2, 10)
     love.window.setMode(wW, wH)
 end
-
 
 function love.update(dt)
     fluid:simulate(dt)
@@ -22,22 +20,22 @@ function love.draw()
     --[[
 
         How this works:
-        Assuming there are grids of i x j. We fill each cell of the grid i x j 
+        Assuming there are grids of i x j. We fill each cell of the grid i x j
         with a rectangle ( cell content ) with a density value of that cell.
         This is done by using a for loop to loop over all the grid cells and their
         corresponding density.
-    
+
         If object is not solid, draw fluid, else draw the solid object
     ]]
     for i = 1, N do
         for j = 1, N do
             if fluid.boundary[i][j] == 0 then
                 local d = math.min(fluid.s[i][j] / 100, 1)
-                love.graphics.setColor(d, d, d, 1) -- Setting color transparency ~ density
-                love.graphics.rectangle("fill", (i-1) * size, (j-1)*size, size, size ) -- Drawing grid elements
+                love.graphics.setColor(d, d, d, 1)                                     -- Setting color transparency ~ density
+                love.graphics.rectangle("fill", (i - 1) * size, (j - 1) * size, size, size) -- Drawing grid elements
             else
                 love.graphics.setColor(0.7, 0.7, 0.7, 1)
-                love.graphics.rectangle("fill", (i-1) * size, (j-1)*size, size, size)
+                love.graphics.rectangle("fill", (i - 1) * size, (j - 1) * size, size, size)
             end
         end
     end
@@ -48,7 +46,7 @@ function love.mousemoved(x, y, dx, dy)
         --[[
             Added +1 because Lua Table/Array Index starts at 1
             ( Algorithm used was assumed to be 0 )
-        ]]  
+        ]]
         fluid:create(x, y, dx, dy, mouseDensity, 20, 20)
     elseif love.mouse.isDown(2) then
         fluid:create(x, y, dx, dy, -mouseDensity, 20, 20)
@@ -56,8 +54,6 @@ function love.mousemoved(x, y, dx, dy)
         fluid:clear(x, y)
     end
 end
-
-
 
 function love.wheelmoved(x, y)
     if y > 0 and mouseDensity < 200 then
@@ -70,14 +66,16 @@ end
 function love.keypressed(key)
     if key == "up" and N < 256 then
         N = N * 2
-        size = wW/N
+        size = wW / N
         fluid:init()
-
+        fluid:solidCircle(N / 2, N / 2, 10)
     elseif key == "down" and N > 32 then
-        N = N /2 
-        size = wW/N
+        N = N / 2
+        size = wW / N
         fluid:init()
+        fluid:solidCircle(N / 2, N / 2, 10)
     elseif key == "c" then
         fluid:init()
+        fluid:solidCircle(N / 2, N / 2, 10)
     end
 end
