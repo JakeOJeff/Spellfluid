2c3698e17b1938d9857e81c381b99d2523351be6
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-12 17:22:30 +0530

    Drew Fluid Cells

--- main.lua
+++ main.lua
@@ -3,7 +3,11 @@ require "utils"
 local fluid = require "fluid"
 
 function love.load()
-    
+    size = 8
+
+    fluid.init()
+
+    love.window.setMode(N * size, N * size)
 end
 
 
@@ -12,5 +16,20 @@ function love.update(dt)
 end
 
 function love.draw()
+    --[[
+
+        How this works:
+        Assuming there are grids of i x j. We fill each cell of the grid i x j 
+        with a rectangle ( cell content ) with a density value of that cell.
+        This is done by using a for loop to loop over all the grid cells and their
+        corresponding density.
     
+    ]]
+    for i = 1, N do
+        for j = 1, N do
+            local d = fluid.density[i][j]
+            love.graphics.setColor(1,1,1,d) -- Setting color transparency ~ density
+            love.graphics.rectangle("fill", (i-1) * size, (j-1)*size, size, size ) -- Drawing grid elements
+        end
+    end
 end
